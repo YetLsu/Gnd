716fc20f728dcc77abc711633283e4133e9e03d9
wyy <wyy@wyydemacbook-pro.local>
2016-08-25 14:23:28 +0800

    Initial Commit

---
+++ gndshop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gndshop
+//
+//  Created by wyy on 16/8/25.
+//  Copyright © 2016年 WYY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gndshop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gndshop
+//
+//  Created by wyy on 16/8/25.
+//  Copyright © 2016年 WYY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
